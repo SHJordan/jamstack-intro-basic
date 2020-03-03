f51bc5e6e83a7a7a74a25470115b931ac45245d1
Restyled.io <commits@restyled.io>
2020-03-03 20:31:35 +0000

    Restyled by clang-format

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 const listRepos = async username => {
-    const repos = await fetch(
-        `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
-    )
-        .then(res => res.json())
-        .catch(error => console.error(error));
+  const repos = await fetch(`https://api.github.com/users/${
+                                username}/repos?type=owner&sort=updated`)
+                    .then(res => res.json())
+                    .catch(error => console.error(error));
 
-    const markup = repos
-        .map(
-            repo => `
+  const markup = repos
+                     .map(repo => `
         <li>
             <a href="${repo.html_url}">${repo.name}</a>
             (⭐ ${repo.stargazers_count})
         </li>
-        `
-        )
-        .join('');
+        `).join('');
 
-    const content = document.getElementById('content');
-    content.innerHTML = `<ul>${markup}</ul>`;
+  const content = document.getElementById('content');
+  content.innerHTML = `<ul>${markup}</ul>`;
 };
 
 listRepos('shjordan');
